14a88b7c13616d9f00904db61a092c73293b4205
Panagiotis Bellias <belliaspanagiotis@gmail.com>
2022-03-04 23:00:21 +0200

    feat(main.py): testing additions

--- app/main.py
+++ app/main.py
@@ -6,3 +6,19 @@ app = FastAPI()
 @app.get("/")
 async def home():
     return {"message": "Welcome to reference letters web system @HUA-DIT!"}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/reference_letter_requests/{reference_letter_request_id}")
+async def read_reference_letter_request(reference_letter_request_id: int):
+    return {"reference_letter_request_id": reference_letter_request_id}
+
+@app.get("/grading_files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
