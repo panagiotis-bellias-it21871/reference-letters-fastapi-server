9c4048d1ecca0a762afcd656e77f09b466f2430f
Panagiotis Bellias <belliaspanagiotis@gmail.com>
2022-03-09 18:22:42 +0200

    build: working on minio integration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 from typing import Optional
 from fastapi import FastAPI
 
+from minio import Minio
+from minio.error import S3Error
+import os
+
 app = FastAPI()
 
+# Create a client with the MinIO server playground, its access key
+# and secret key.
+client = Minio(
+    str(os.getenv("MIN_IO_SERVER", default="localhost")),
+    access_key=os.getenv("MIN_IO_ACCESS_KEY"),
+    secret_key=os.getenv("MIN_IO_SECRET_KEY")
+)
+# Make 'asiatrip' bucket if not exist.
+bucket = os.getenv("MIN_IO_BUCKET_NAME")
+found = client.bucket_exists(bucket)
+if not found:
+    client.make_bucket(bucket)
+else:
+    print(f"Bucket {bucket} already exists")
 
 @app.get("/")
 def read_root():
@@ -23,3 +41,21 @@ async def read_reference_letter_request(reference_letter_request_id: int, q: Opt
 @app.get("/grading_files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+@app.get("/test-minio")
+async def test_minio():
+    try:
+        # Upload '/home/user/Photos/asiaphotos.zip' as object name
+        # 'asiaphotos-2015.zip' to bucket 'asiatrip'.
+        client.fput_object(
+            bucket, "test-09-03-2022.zip", "../README.md",
+        )
+        print(
+            f"'README.md' is successfully uploaded as "
+            "object 'test-09-03-2022.zip' to bucket {bucket}."
+        )
+        message = "Success!"
+    except S3Error as exc:
+        message = f"error occurred. {exc}"
+        print(message)
+    return {"message": message}
