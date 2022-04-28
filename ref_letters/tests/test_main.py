603c2087faf4dd7e966c439e8788bfac81cd5d5e
pan-bellias <belliaspan@gmail.com>
2022-04-28 19:55:57 +0300

    working on unit tests

--- ref_letters/tests/test_main.py
+++ ref_letters/tests/test_main.py
@@ -1,13 +1,18 @@
-from fastapi.testclient import TestClient
+from fastapi.testclient import TestClient # Import TestClient.
 # from sqlalchemy import create_engine
 # from sqlalchemy.orm import sessionmaker
-import json
+# import json
 
 # from ..database import Base
 from ..main import app
-client = TestClient(app)
 
+client = TestClient(app) # TestClient created passing the FastAPI application.
 
+"""
+Functions created with name that starts with test_ (pytest convention).
+TestClient object is used the same way as with requests.
+Simple assert statements written that check (pytest).
+"""
 def test_read_main():
     response = client.get("/ping")
     assert response.status_code == 200
