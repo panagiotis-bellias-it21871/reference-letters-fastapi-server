37dcd4b2fa69fefac5129db0a8bf22930b894aab
pan-bellias <belliaspan@gmail.com>
2022-05-07 22:52:15 +0300

    db changes (crud) finished - ready

--- ref_letters/db.py
+++ ref_letters/db.py
@@ -1,3 +1,42 @@
+import databases
+import sqlalchemy
+
+DATABASE_URL = "sqlite:///./dev.db"
+metadata = sqlalchemy.MetaData()
+database = databases.Database(DATABASE_URL)
+reference_letter_request_db = sqlalchemy.Table(
+    "reference_letter_request",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String(500)),
+    sqlalchemy.Column("is_approved", sqlalchemy.Boolean),
+    sqlalchemy.Column("is_declined", sqlalchemy.Boolean),
+    sqlalchemy.Column("is_pending", sqlalchemy.Boolean),
+)
+
+student_db = sqlalchemy.Table(
+    "student",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String(500)),
+    sqlalchemy.Column("school_id", sqlalchemy.String(125)),
+    sqlalchemy.Column("email", sqlalchemy.String(750)),
+)
+
+teacher_db = sqlalchemy.Table(
+    "teacher",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String(500)),
+    sqlalchemy.Column("email", sqlalchemy.String(750)),
+)
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+metadata.create_all(engine)
+
 """
 import os
 
@@ -20,7 +59,7 @@ async def init_db():
 
 
 # SQLAlchemy specific code, as with any other app
-DATABASE_URL = "sqlite:///./test.db"
+#DATABASE_URL = "sqlite:///./test.db"
 # DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 #database = databases.Database(DATABASE_URL)
--- ref_letters/main.py
+++ ref_letters/main.py
@@ -1,32 +1,19 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Form
 
-'''
-from sqlalchemy.future import select
-from sqlalchemy.ext.asyncio import AsyncSession
-from typing import List
-
-from ref_letters.db import get_session, init_db
-from ref_letters.models import ReferenceLetterRequest, ReferenceLetterRequestCreate, Student, StudentCreate, Teacher, TeacherCreate
-'''
-from .schemas import User #ReferenceLetterRequest
 from .utils import get_current_user
-#from .db import database
+from .db import database, reference_letter_request_db, student_db, teacher_db
 
-# YT1
-from .schemas import fakedb_rl_requests, fakedb_students, fakedb_teachers, ReferenceLetterRequest, Student, Teacher
+from .schemas import ReferenceLetterRequest, Student, Teacher, User
 
 app = FastAPI()
 
-'''
 @app.on_event("startup")
-async def on_startup():
+async def connect():
     await database.connect()
-    # await init_db()
 
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
-'''
 
 @app.get("/")
 def read_root():
@@ -40,104 +27,130 @@ async def pong():
 async def read_users_me(current_user: User = Depends(get_current_user)):
     return current_user
 
-# YT1
-#async
-@app.get("/rl_requests") #, response_model=List[ReferenceLetterRequest])
-def get_rl_requests():#session: AsyncSession = Depends(get_session)):
-    return fakedb_rl_requests
-    '''
-    result = await session.execute(select(ReferenceLetterRequest))
-    rl_requests = result.scalars().all()
-    return [ReferenceLetterRequest(name=rl_request.name, description=rl_request.description, id=rl_request.id) for rl_request in rl_requests]
-    '''
+# YT2
+@app.post("/language/")
+async def language(name: str = Form(...), type: str = Form(...)):
+    return {"name": name, "type": type}
+
+@app.get("/rl_requests")
+async def get_rl_requests():
+    query = reference_letter_request_db.select()
+    all_get = await database.fetch_all(query)
+    return all_get
 
 @app.get("/rl_requests/{rl_request_id}")
-def get_a_course(rl_request_id: int):
-    rl_request = rl_request_id - 1
-    return fakedb_rl_requests[rl_request]
+async def get_a_rl_request(rl_request_id: int):
+    query = reference_letter_request_db.select().where(reference_letter_request_db.c.id == rl_request_id)
+    user = await database.fetch_one(query)
+    return {**user}
 
-#async
 @app.post("/rl_requests")
-def add_rl_request(rl_request: ReferenceLetterRequest):#ReferenceLetterRequestCreate, session: AsyncSession = Depends(get_session)):
-    fakedb_rl_requests.append(rl_request.dict())
-    return fakedb_rl_requests[-1]
-    '''
-    rl_request = ReferenceLetterRequest(name=rl_request.name, description=rl_request.description)
-    session.add(rl_request)
-    await session.commit()
-    await session.refresh(rl_request)
-    return rl_request
-    '''
+async def add_rl_request(rl_request: ReferenceLetterRequest):
+    query = reference_letter_request_db.insert().values(
+        name=rl_request.name,
+        is_approved=rl_request.is_approved,
+        is_declined=rl_request.is_declined,
+        is_pending=rl_request.is_pending
+    )
+    record_id = await database.execute(query)
+    query = reference_letter_request_db.select().where(reference_letter_request_db.c.id == record_id)
+    row = await database.fetch_one(query)
+    return {**row}
+
+@app.put("/rl_requests/{rl_request_id}")
+async def update_rl_request(rl_request_id: int, rl_request: ReferenceLetterRequest):
+    query = reference_letter_request_db.update().where(reference_letter_request_db.c.id == rl_request_id).values(
+        name=rl_request.name,
+        is_approved=rl_request.is_approved,
+        is_declined=rl_request.is_declined,
+        is_pending=rl_request.is_pending
+    )
+    record_id = await database.execute(query)
+    query = reference_letter_request_db.select().where(reference_letter_request_db.c.id == record_id)
+    row = await database.fetch_one(query)
+    return {**row}
 
 @app.delete("/rl_requests/{rl_request_id}")
-def delete_rl_request(rl_request_id: int):
-    fakedb_rl_requests.pop(rl_request_id - 1)
-    return {"task": "deletion successful"}
+async def delete_rl_request(rl_request_id: int):
+    query = reference_letter_request_db.delete().where(reference_letter_request_db.c.id == rl_request_id)
+    return await database.execute(query)
 
-# YT1
-#async
-@app.get("/students") #, response_model=List[Student])
-def get_students():#session: AsyncSession = Depends(get_session)):
-    return fakedb_students
-    '''
-    result = await session.execute(select(Student))
-    students = result.scalars().all()
-    return [Student(name=student.name, email=student.email, school_id=student.school_id, id=student.id) for student in students]
-    '''
+@app.get("/students")
+async def get_students():
+    query = student_db.select()
+    all_get = await database.fetch_all(query)
+    return all_get
 
 @app.get("/students/{student_id}")
-def get_a_student(student_id: int):
-    student = student_id - 1
-    return fakedb_students[student]
+async def get_a_student(student_id: int):
+    query = student_db.select().where(student_db.c.id == student_id)
+    user = await database.fetch_one(query)
+    return {**user}
 
-#async
 @app.post("/students")
-def add_student(student: Student):#StudentCreate, session: AsyncSession = Depends(get_session)):
-    fakedb_students.append(student.dict())
-    return fakedb_students[-1]
-    '''
-    student = Student(name=student.name, email=student.email, school_id=student.school_id)
-    session.add(student)
-    await session.commit()
-    await session.refresh(rl_request)
-    return student
-    '''
+async def add_student(student: Student):
+    query = student_db.insert().values(
+        name=student.name,
+        school_id=student.school_id,
+        email=student.email
+    )
+    record_id = await database.execute(query)
+    query = student_db.select().where(student_db.c.id == record_id)
+    row = await database.fetch_one(query)
+    return {**row}
+
+@app.put("/students/{student_id}")
+async def update_student(student_id: int, student: Student):
+    query = student_db.update().where(student_db.c.id == student_id).values(
+        name=student.name,
+        school_id=student.school_id,
+        email=student.email
+    )
+    record_id = await database.execute(query)
+    query = student_db.select().where(student_db.c.id == record_id)
+    row = await database.fetch_one(query)
+    return {**row}
 
 @app.delete("/students/{student_id}")
-def delete_student(student_id: int):
-    fakedb_students.pop(student_id - 1)
-    return {"task": "deletion successful"}
+async def delete_student(student_id: int):
+    query = student_db.delete().where(student_db.c.id == student_id)
+    return await database.execute(query)
 
-# YT1
-#async
-@app.get("/teachers") #, response_model=List[Teacher])
-def get_teachers():#session: AsyncSession = Depends(get_session)):
-    return fakedb_teachers
-    '''
-    result = await session.execute(select(Teacher))
-    teachers = result.scalars().all()
-    return [Teacher(name=teacher.name, email=teacher.email, degree=teacher.degree, id=teacher.id) for teacher in teachers]
-    '''
+@app.get("/teachers")
+async def get_teachers():
+    query = teacher_db.select()
+    all_get = await database.fetch_all(query)
+    return all_get
 
 @app.get("/teachers/{teacher_id}")
-def get_a_teacher(teacher_id: int):
-    teacher = teacher_id - 1
-    return fakedb_teachers[teacher]
+async def get_a_teacher(teacher_id: int):
+    query = teacher_db.select().where(teacher_db.c.id == teacher_id)
+    user = await database.fetch_one(query)
+    return {**user}
 
-#async
 @app.post("/teachers")
-def add_teacher(teacher: Teacher):#TeacherCreate, session: AsyncSession = Depends(get_session)):
-    fakedb_teachers.append(teacher.dict())
-    return fakedb_teachers[-1]
-    '''
-    teacher = Teacher(name=teacher.name, email=teacher.email, degree=teacher.degree)
-    session.add(teacher)
-    await session.commit()
-    await session.refresh(teacher)
-    return teacher
-    '''
+async def add_teacher(teacher: Teacher):
+    query = teacher_db.insert().values(
+        name=teacher.name,
+        email=teacher.email
+    )
+    record_id = await database.execute(query)
+    query = teacher_db.select().where(teacher_db.c.id == record_id)
+    row = await database.fetch_one(query)
+    return {**row}
+
+@app.put("/teachers/{teacher_id}")
+async def update_teacher(teacher_id: int, teacher: Teacher):
+    query = teacher_db.update().where(teacher_db.c.id == teacher_id).values(
+        name=teacher.name,
+        email=teacher.email
+    )
+    record_id = await database.execute(query)
+    query = teacher_db.select().where(teacher_db.c.id == record_id)
+    row = await database.fetch_one(query)
+    return {**row}
 
 @app.delete("/teachers/{teacher_id}")
-def delete_teacher(teacher_id: int):
-    fakedb_teachers.pop(teacher_id - 1)
-    return {"task": "deletion successful"}
+async def delete_teacher(teacher_id: int):
+    query = teacher_db.delete().where(teacher_db.c.id == teacher_id)
+    return await database.execute(query)
