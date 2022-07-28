cc9bfc9be7c0b3db4dc00f2bb1f87aeb908a7827
panagiotis-bellias-it21871 <it21871@hua.gr>
2022-07-28 13:03:55 +0300

    added user authentication - issue with db

--- ref_letters/main.py
+++ ref_letters/main.py
@@ -1,10 +1,15 @@
 #import os
 from dotenv import load_dotenv
-from fastapi import FastAPI, Form #, Depends
+from fastapi import Depends, FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
 
-from .routers import rl_requests, students, teachers #, keycloak_user_handling
+from .schemas import User, create_db_and_tables
+from .user_manager import (UserCreate, UserRead, UserUpdate, auth_backend,
+                          current_active_user, fastapi_users)
+
 from .db import database
+from .routers import (rl_requests, students,  # , keycloak_user_handling
+                      teachers)
 
 load_dotenv(verbose=True)
 
@@ -26,9 +31,26 @@ app.include_router(students.router)
 app.include_router(teachers.router)
 #app.include_router(keycloak_user_handling.router)
 
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend, requires_verification=True),
+    prefix="/auth/jwt",
+    tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"] 
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate, requires_verification=True),
+    prefix="/users",
+    tags=["users"]
+)
+
 @app.on_event("startup")
 async def connect():
     await database.connect()
+    await create_db_and_tables()
 
 @app.on_event("shutdown")
 async def shutdown():
@@ -37,6 +59,10 @@ async def shutdown():
 @app.get("/")
 def read_root():
     return {"greetings": "Welcome to FastAPI Python"}
+
+@app.get("/authenticated-route")
+async def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
 
 @app.get("/ping")
 async def ping():
--- ref_letters/schemas.py
+++ ref_letters/schemas.py
@@ -1,9 +1,14 @@
-from lib2to3.pgen2.token import OP
-from typing import Optional
+from typing import AsyncGenerator, Optional
 
+from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
+from fastapi_users_db_sqlalchemy import AsyncSession, SQLAlchemyUserDatabase
 from pydantic import BaseModel
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from .db import engine
+
 Base: DeclarativeMeta = declarative_base()
 
 class ReferenceLetterRequest(BaseModel):
@@ -37,6 +42,19 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
     teacher: bool
     admin: bool
     disabled: Optional[bool] = None
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
 
 class UserInDB(User):
     hashed_password: str
